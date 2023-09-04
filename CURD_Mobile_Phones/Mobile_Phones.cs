66be2407eeb6fba7be43f80e1428fec7fcb76837
RAJ <RAJ@LAPTOP-IPORQCI4>
2023-09-04 14:02:00 +0530

    Add project files.

---
+++ CURD_Mobile_Phones/Mobile_Phones.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURD_Mobile_Phones
+{
+    public class Mobile
+    {
+        int id;
+        String name;
+        int price;
+        public Mobile(int id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.Price = price;
+        }
+        public override string ToString()
+        {
+            return id + " :" + name + " " + price;
+        }
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+    }
+
+    public class MobileImplementation
+    {
+        Mobile[] mobile;
+        int count;
+        public MobileImplementation()
+        {
+            mobile = new Mobile[5];
+            count = 0;
+        }
+        public void AddMobile(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Mobile id");
+                int mid = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Mobile name");
+                String mname = Console.ReadLine();
+                Console.WriteLine("Entre Mobile price");
+                int mprice = int.Parse(Console.ReadLine());
+                mobile[count] = new Mobile(mid, mname, mprice);
+                count++;
+            }
+        }
+        public void ShowAllMobile()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (mobile[i] != null)
+                {
+                    Console.WriteLine(mobile[i]);
+                }
+            }
+        }
+        public void SearchMobileById(int id)
+        {
+            for (int i = 0; i < mobile.Length; i++)
+            {
+                if (mobile[i] != null)
+                {
+                    if (mobile[i].Id == id)
+                    {
+                        Console.WriteLine(mobile[i]);
+                    }
+                    if (mobile[i].Id != id)
+                        {
+                            Console.WriteLine("Mobile Not Available");
+                        }
+                }
+            }
+        }
+        public void UpadteMobile(int id, int newprice)
+        {
+            for (int i = 0; i < mobile.Length; i++)
+            {
+                if (mobile[i] != null)
+                {
+                    if (mobile[i].Id == id)
+                    {
+                        mobile[i].Price = newprice;
+                    }
+                }
+            }
+        }
+        public void DeleteMobile(int id)
+        {
+            for (int i = 0; i < mobile.Length; i++)
+            {
+                if (mobile[i] != null)
+                {
+                    if (mobile[i].Id == id)
+                    {
+                        mobile[i] = null;
+                    }
+                    if (mobile[i].Id != id)
+                    {
+                        Console.WriteLine("Mobile Not available");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ CURD_Mobile_Phones/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURD_Mobile_Phones
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MobileImplementation mobile = new MobileImplementation();
+            char ch = ' ';
+            do
+            {
+                Console.WriteLine("-------------------");
+                Console.WriteLine("1.Insert\n2.Show\n3.Update\n4.Delete\n5.Serach");
+                Console.WriteLine("Enter Your choice Number:");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("How many products you want to add:");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        mobile.AddMobile(n);
+                        break;
+                    case 2:
+                        Console.WriteLine("-----------------------------");
+                        mobile.ShowAllMobile();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the id for update:");
+                        int mid = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the new price:"); 
+                        int price = int.Parse(Console.ReadLine());
+                        mobile.UpadteMobile(mid,price);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the id for delete:");
+                        int dbid = Convert.ToInt32(Console.ReadLine());
+                        mobile.DeleteMobile(dbid);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter id for search:");
+                        int id = int.Parse(Console.ReadLine());
+                        mobile.SearchMobileById(id);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Operation");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue then enter (y/Y....");
+                ch = Convert.ToChar(Console.ReadLine());
+
+            } while (ch == 'y' || ch == 'Y');
+
+
+
+        }
+    }
+}
+
+
+
+    
+
+
